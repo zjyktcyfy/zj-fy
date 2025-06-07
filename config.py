d36be143ac497b9cb46e54dfb32eeef27e668428
zjyktcyfy <ykfy8806518@163.com>
2025-06-07 15:30:03 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,46 @@
+ip_version_priority = "ipv6"
+
+source_urls = [
+    "https://zj.yangfeiyue.dpdns.org/txt/Zhejiang_dianxin.txt",
+    "https://sh.yangfeiyue.dpdns.org/txt/Shanghai_dianxin.txt",
+    "https://gd.yangfeiyue.dpdns.org/txt/Guangdong_dianxin.txt",
+    "https://bj.yangfeiyue.dpdns.org/txt/Beijing_liantong.txt",
+    "https://tj.yangfeiyue.dpdns.org/txt/Tianjin_liantong.txt",
+    "https://js.yangfeiyue.dpdns.org/txt/Jiangsu_dianxin.txt",
+    "https://cq.yangfeiyue.dpdns.org/txt/Chongqing_liantong.txt",
+    "https://shx.yangfeiyue.dpdns.org/txt/Shaanxi_dianxin.txt",
+    "https://jx.yangfeiyue.dpdns.org/txt/Jiangxi_dianxin.txt",
+    "https://fj.yangfeiyue.dpdns.org/txt/Fujian_dianxin.txt",
+    "https://ah.yangfeiyue.dpdns.org/txt/Anhui_dianxin.txt",
+    "https://hun.yangfeiyue.dpdns.org/txt/Hunan_dianxin.txt",
+    "https://sc.yangfeiyue.dpdns.org/txt/Sichuan_dianxin.txt",
+    "https://gx.yangfeiyue.dpdns.org/txt/Guangxi_dianxin.txt",
+    "https://sd.yangfeiyue.dpdns.org/txt/Shandong_dianxin.txt",
+    "https://hb.yangfeiyue.dpdns.org/txt/Hubei_dianxin.txt",
+    "https://heb.yangfeiyue.dpdns.org/txt/Hebei_liantong.txt",
+    "https://sx.yangfeiyue.dpdns.org/txt/Shanxi_liantong.txt",
+    "https://hn.yangfeiyue.dpdns.org/txt/Henan_dianxin.txt",
+    "https://ln.yangfeiyue.dpdns.org/txt/Liaoning_liantong.txt",
+    "https://hlj.yangfeiyue.dpdns.org/txt/Heilongjiang_liantong.txt",
+    "https://gd1.yangfeiyue.dpdns.org/txt/Guang_dian.txt",
+    "https://cai1.yangfeiyue.dpdns.org/speedtest/txt/NEW.m3u",
+]
+
+url_blacklist = [
+    "epg.pw/stream/",
+]
+
+announcements = [
+    {
+        "channel": "公告",
+        "entries": [
+            {"name": "直播源均来自网络，不得商用，仅供学习交流使用。", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+            {"name": "更新日期", "url": "https://ali-m-l.cztv.com/channels/lantian/channel006/1080p.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+            {"name": None, "url": "https://live-auth.51kandianshi.com/szgd/csztv4.m3u8", "logo": "https://live1.yangfeiyue.dpdns.org/tv/武术台.png"},
+  ]
+    }
+]
+
+epg_urls = [
+    "https://assets.live1dnow.com/epg.xml",
+]

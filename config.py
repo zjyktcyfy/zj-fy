6f10393753e2e32b2bcd791960aa2180c3a469e6
zjyktcyfy <ykfy8806518@163.com>
2025-06-24 19:48:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ source_urls = [
     "https://scc.yangfeiyue.dpdns.org/txt/Sichuan_dianxin.txt",
     "https://gxx.yangfeiyue.dpdns.org/txt/Guangxi_dianxin.txt",
     "https://hbb.yangfeiyue.dpdns.org/txt/Hubei_dianxin.txt",
-    "https://heeb.yangfeiyue.dpdns.org/txt/Hebei_dianxin.txt",
+    "https://hebb.yangfeiyue.dpdns.org/txt/Hebei_dianxin.txt",
     "https://heeb.yangfeiyue.dpdns.org/txt/Hebei_liantong.txt",
     "https://sxx.yangfeiyue.dpdns.org/txt/Shanxi_liantong.txt",
     "https://hhn.yangfeiyue.dpdns.org/txt/Henan_dianxin.txt",

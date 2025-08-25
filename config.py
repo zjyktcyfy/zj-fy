b09eeed421b7299c93e133a3e75a6bc9f04a9522
zjyktcyfy <ykfy8806518@163.com>
2025-08-25 15:20:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -31,6 +31,7 @@ source_urls = [
     "https://gzz.yangfeiyue.dpdns.org/txt/Guizhou_dianxin.txt",
     "https://nxx.yangfeiyue.dpdns.org/txt/Ningxia_dianxin.txt",
     "https://ggd.yangfeiyue.dpdns.org/txt/Guang_dian.txt",
+    "https://krsk.yangfeiyue.dpdns.org/txt/KR_SK.txt",
     "https://proxy.lalifeier.eu.org/https://raw.githubusercontent.com/zjyktcyfy/zj-fy/refs/heads/main/txt/ZJYD.m3u",
 ]
 
@@ -52,3 +53,4 @@ announcements = [
 epg_urls = [
     "https://assets.live1dnow.com/epg.xml",
 ]
+
